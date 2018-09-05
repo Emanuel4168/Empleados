cbd323e444aa956cc3c663bd34840369c3530744
Unknown <emanuel.l4168@gmail.com>
2018-09-05 16:15:45 -0600

    Clases Principales
    Creación de las clases principales a utilizar y su estructura básica

---
+++ Empleados/src/empleados/Main.java
@@ -0,0 +1,26 @@
+package empleados;
+
+import java.util.Scanner;
+
+public class Main 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner scan=new Scanner(System.in);
+		int n=0,inicio;
+		int [] empleados=new int[n];
+
+	}
+	
+	
+	private void displayMenu()
+	{
+		System.out.println("------Menú------");
+		System.out.println("1. Dar de alta un cliente");
+		System.out.println("2. Dar de baja un cliente");
+		System.out.println("3. Consultar un cliente");
+		System.out.println("4. Salir");
+	}
+
+}
