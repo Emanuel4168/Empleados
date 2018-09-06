ea3227567dda0e321bfe5713d78a82b332ca72d9
Unknown <emanuel.l4168@gmail.com>
2018-09-06 06:01:05 -0600

    Avance en proceso de ordenamiento lógico
    Se crearon las bases de un sistema de ordenamiento lógico al registrar empleados

--- Empleados/src/empleados/Empleado.java
+++ Empleados/src/empleados/Empleado.java
@@ -1,5 +1,21 @@
 package empleados;
 
-public class Empleado {
+public class Empleado 
+{
+	public int clave;
+	public String nombre;
+	public int edad;
+	public char estadoCivil;
+	public int siguiente;
+	
+	public Empleado(int clave, String nombre, int edad, char estadoCivil, int siguiente) 
+	{
+		super();
+		this.clave = clave;
+		this.nombre = nombre;
+		this.edad = edad;
+		this.estadoCivil = estadoCivil;
+		this.siguiente = siguiente;
+	}
 
 }
--- Empleados/src/empleados/Main.java
+++ Empleados/src/empleados/Main.java
@@ -4,13 +4,85 @@ import java.util.Scanner;
 
 public class Main 
 {
-
+	private static Scanner scan=new Scanner(System.in);
+	private static int inicio=-1;
+	private static Empleado empleadoMayor;
+	private static Empleado empleadoMenor;
+	private static int numEmpleados=5;
+	private static Empleado [] empleados=new Empleado[numEmpleados];
+	
+	
 	public static void main(String[] args)
 	{
-		Scanner scan=new Scanner(System.in);
-		int n=0,inicio;
-		int [] empleados=new int[n];
-
+	    
+	}
+	
+	
+	
+	private Empleado guardarEmpleado()
+	{	
+        int clave,edad;
+		String nombre;
+		char estadoCivil;
+        System.out.println("Ingrese Clave del empleado");
+		clave=scan.nextInt();
+		System.out.println("Ingrese Nombre del empleado");
+		nombre=scan.nextLine();
+		System.out.println("Ingrese edad del empleado");
+		edad=scan.nextInt();
+		System.out.println("Ingrese Estado Civil del empleado");
+		estadoCivil=scan.nextLine().charAt(0);
+		return null;
+	}
+	
+	
+	private int calcularSiguiente(Empleado empleado)
+	{
+		int siguiente=-1;
+		if(empleados[0]==null)
+		{
+			this.empleadoMayor=empleado;
+			this.empleadoMenor=empleado;
+			this.inicio=buscarPosicion(empleado);
+		}
+		else
+		{
+			if(empleado.nombre.compareTo(empleadoMenor.nombre)==-1)
+			{
+				siguiente=buscarPosicion(empleadoMenor);
+				this.inicio=buscarPosicion(empleado);
+				this.empleadoMenor=empleado;
+			}
+			else if(empleado.nombre.compareTo(empleadoMayor.nombre)==1)
+				this.empleadoMayor=empleado;
+			else
+			{
+				int posicionLogica=empleadoMenor.siguiente;
+				while(posicionLogica!=-1)
+				{
+					if(empleados[posicionLogica].nombre.compareTo(empleado.nombre)==1)
+					{
+						siguiente=buscarPosicion(empleados[posicionLogica]);
+						break;
+					}
+					posicionLogica=empleados[posicionLogica].siguiente;
+				}
+			}	
+		}
+		return siguiente;
+		
+	}
+	
+	
+	private int buscarPosicion(Empleado empleado)
+	{
+		int posicion=-1;
+		for(int i=0;i<empleados.length;i++)
+		{
+			if(empleados[i].clave==empleado.clave)
+				posicion=i;
+		}
+		return posicion;
 	}
 	
 	
